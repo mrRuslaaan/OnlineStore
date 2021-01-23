5d0559064ec7c1093ad966f13e3a85b5c62f8d73
Руслан Аннаев <annaev.ruslan@gmail.com>
2021-01-23 17:43:58 +0300

    view of List of Workers

---
+++ OnlineStore/Data/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineStore.Models;
+
+namespace OnlineStore.Data
+{
+    public static class TestData
+    {
+        public static List<Worker> Workers = new List<Worker>()
+        {
+            new Worker() { ID = 1, Name = "Александр", Surname ="Пушкин", Patronymic = "Сергеевич", Age = 25 },
+            new Worker() { ID = 2, Name = "Федор", Surname ="Достоевский", Patronymic = "Михайлович", Age = 32 },
+            new Worker() { ID = 3, Name = "Лев", Surname ="Толстой", Patronymic = "Николаевич", Age = 33 },
+        };
+    }
+}
--- OnlineStore/Startup.cs
+++ OnlineStore/Startup.cs
@@ -30,11 +30,6 @@ namespace OnlineStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
