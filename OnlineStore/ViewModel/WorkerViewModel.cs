fa3f95da865d3334be735fdaf12dc70b0f0f2017
Руслан Аннаев <annaev.ruslan@gmail.com>
2021-01-28 20:51:06 +0300

    Edit functionality

---
+++ OnlineStore/ViewModel/WorkerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.ViewModel
+{
+    public class WorkerViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+    }
+}
