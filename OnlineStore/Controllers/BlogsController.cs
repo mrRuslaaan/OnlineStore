fd991ddce43f70d96ebddbfd97b98f0d052aa5c7
Руслан Аннаев <annaev.ruslan@gmail.com>
2021-01-29 17:45:14 +0300

    BlogController, BlogView

---
+++ OnlineStore/Controllers/BlogsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OnlineStore.Data;
+using OnlineStore.Models;
+using OnlineStore.Infrastructure.Interfaces;
+using OnlineStore.Infrastructure.Services;
+using System.Threading.Tasks;
+using OnlineStore.ViewModel;
+
+namespace OnlineStore.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly IBlog _Blogs;
+        public BlogsController (IBlog Blogs) => _Blogs = Blogs;
+
+        public IActionResult Index()
+        {
+            return View(_Blogs.Get());
+        }
+    }
+}
