627a0fc1cea702aa19990c35e7ffd70128586a93
Руслан Аннаев <annaev.ruslan@gmail.com>
2021-01-29 17:14:47 +0300

    Some functionality for blog service and registaration of the service at the service container

---
+++ OnlineStore/Infrastructure/Services/IBlogService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineStore.Infrastructure.Interfaces;
+using OnlineStore.Models;
+using OnlineStore.Data;
+
+namespace OnlineStore.Infrastructure.Services
+{
+    public class IBlogService : IBlog
+    {
+        private List<Blog> _Blogs = TestData.Blogs;
+        public int Add(Blog blog)
+        {
+            if (blog is null)
+                throw new ArgumentNullException(nameof(blog));
+
+
+
+            return blog.ID;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Blog> Get()
+        {
+            return _Blogs;
+        }
+
+        public Blog Get(int id)
+        {
+            if (id < 0)
+                throw new ArgumentException();
+
+            return _Blogs.FirstOrDefault(w => w.ID == id);
+        }
+
+        public int MaxID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
