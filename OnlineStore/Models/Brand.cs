8c3211724c76563b98611c57e8592cd0d8cc8c25
Руслан Аннаев <annaev.ruslan@gmail.com>
2021-02-02 16:01:55 +0300

    correst mistakes with property Name in models of Brand,Category,Product

--- OnlineStore/Models/Brand.cs
+++ OnlineStore/Models/Brand.cs
@@ -8,6 +8,6 @@ namespace OnlineStore.Models
     public class Brand
     {
         public int Id { get; set; }
-        string Name { get; set; }
+        public string Name { get; set; }
     }
 }
