5fa91710feed6b87539bfcf54d68067e77429b7c
Руслан Аннаев <annaev.ruslan@gmail.com>
2021-02-03 14:17:38 +0300

    Visual Component of Brands

---
+++ OnlineStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Infrastructure.Interfaces;
+using OnlineStore.ViewModel;
+
+namespace OnlineStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProduct _Products;
+        public BrandsViewComponent(IProduct Products) => _Products = Products;
+
+        public IViewComponentResult Invoke()
+        {
+            var brands = _Products.GetBrands();
+
+            var brandsEnum = brands.Select(brand => new BrandViewModel
+            {
+                Id = brand.Id,
+                Name = brand.Name
+            });
+
+            return View(brandsEnum);
+        }
+    }
+}
