6759786542241bd6ede770f0c11b4e9b1a1de04a
Руслан Аннаев <annaev.ruslan@gmail.com>
2021-02-03 17:30:17 +0300

    CatalogController

---
+++ OnlineStore/ViewModel/CatalogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.ViewModel
+{
+    public class CatalogViewModel
+    {
+        public int? CategoryID { get; set; }
+        public int? BrandID { get; set; }
+
+        public IEnumerable<ProductViewModel> Products { get; set; } 
+    }
+}
