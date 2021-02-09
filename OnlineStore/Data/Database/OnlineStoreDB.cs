5802103e05e4fc749b6a5f1c78cabc2d0d4713a8
Руслан Аннаев <annaev.ruslan@gmail.com>
2021-02-09 16:58:31 +0300

    db context

---
+++ OnlineStore/Data/Database/OnlineStoreDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Models;
+
+namespace OnlineStore.DAL
+{
+    public class OnlineStoreDB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        OnlineStoreDB(DbContextOptions<OnlineStoreDB> options) : base(options) { }
+    }
+}
